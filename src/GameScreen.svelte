610b61f177ab7cecca7e6ec07e3d6e71be1d0508
Daniel Vergeylen <daniel.vergeylen@sigmadelta.io>
2021-06-30 17:36:10 +0200

    feat: added basic scenario loading (fetch API)

--- src/GameScreen.svelte
+++ src/GameScreen.svelte
@@ -1,1 +1,43 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { gameState } from './stores/state';
+  import { appStatus } from './stores/appStatus';
+  import { GameStatus } from './enums';
+
+  let scenariosPromises: Promise<Array<Object>>;
+
+  onMount(async () => {
+      const promises: Array<Promise<Object>> = Array.from(Array(10).keys()).map(async (i) => {
+        const scenarioFile = await fetch(`scenarios/chapitre${String(i).padStart(2, '0')}.json`)
+        const scenario = await scenarioFile.text();
+
+        if (scenario.length > 0) {
+          return JSON.parse(scenario);
+        } else {
+          throw new Error(`Impossible de lire le scénario n°${String(i).padStart(2, '0')}`);
+        }
+      });
+
+      scenariosPromises = new Promise(async (resolve, reject) => {
+        try {
+          const results = await Promise.all(promises);
+          resolve(results);
+        } catch (e) {
+          reject(e);
+        }
+      });
+  });
+
+  // TODO: plutôt que d'avoir un array de scenarios, il serait utile d'en avoir un objet key → value pour chacun, adressable par scenarios[i]
+
+</script>
+
 <p>Game Screen!</p>
+
+{#await scenariosPromises}
+  <p>Chargement du jeu, veuillez patienter</p>
+{:then scenario}
+  <p>Les scénarios sont chargés</p>
+{:catch error}
+  <p style="color: red">Une erreur est survenue: {error}</p>
+{/await}
--- src/global.d.ts
+++ src/global.d.ts
@@ -6,3 +6,38 @@ interface GameState {
     [chapterId: string]: Array<string>,
   }
 }
+
+interface DialogNode {
+  id: string,
+  text: {
+    fr: string | {
+      m: string,
+      f: string,
+    },
+    en?: string,
+  },
+  nextNodes: Array<string>,
+  incomingNodes?: Array<string>,
+  character: "Player" | "NPC1",
+  attribution?: {
+    leader?: number,
+    bricoleur?: number,
+    coequipier?: number,
+    planificateur?: number,
+    idealiste?: number,
+    creatif?: number,
+    audacieux?: number,
+    explorateur?: number,
+  }
+  imagePath?: string,
+}
+
+interface Scenario {
+  metadata: {
+    part: string,
+    chapter: string,
+  };
+  dialogNodes: {
+    [id: string]: DialogNode,
+  };
+}
