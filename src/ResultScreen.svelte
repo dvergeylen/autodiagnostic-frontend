9a8c3f46a53d2d2972805325f3fd92ea39f2ef88
Daniel Vergeylen <daniel.vergeylen@sigmadelta.io>
2021-07-08 14:51:01 +0200

    feat: added basic Result Screen (only planificateur for now)

--- src/ResultScreen.svelte
+++ src/ResultScreen.svelte
@@ -1,1 +1,95 @@
-<p>Result Screen!</p>
+
+<script lang="ts">
+  import { onMount } from "svelte";
+  import Planificateur from './profiles/Planificateur.svelte';
+
+  const profiles = {
+    planificateur: Planificateur,
+  }
+  let selectedProfile = 'planificateur';
+
+  onMount(() => {
+   window.scrollTo(0, 0);
+  });
+</script>
+<main>
+  <div id="congratulations">
+    <h1>Félicitations!</h1>
+    <p>
+      Belle aventure ! Félicitations et merci d’avoir joué ! <br />
+      Voici les résultats du test :
+    </p>
+  </div>
+
+  <div id="profile">
+    <h3>Ton profil:</h3>
+    <h2>Le planificateur</h2>
+  </div>
+
+  <div id="graph">
+    <img src="/assets/profile_results.svg" alt="Graphe des résultats"/>
+  </div>
+
+  <div id="description">
+    <svelte:component this={profiles[selectedProfile]} score={5}/>
+  </div>
+</main>
+
+<style lang="scss">
+
+  #profile {
+    color: white;
+    background-color: #800000;
+    margin-top: 1em;
+    margin-bottom: 2em;
+    h2 {
+      margin-top: 0.15em;
+      margin-bottom: 0.5em;
+      font-size: 2em;
+    }
+    h3 {
+      margin-bottom: 0;
+    }
+  }
+
+  #graph {
+    img {
+      width: 80%;
+      max-width: 25em;
+    }
+  }
+
+  #description {
+    text-align: left;
+    max-width: 35em;
+    margin: auto;
+  }
+
+  // Commons
+  main {
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+
+    // Phones
+    @media (max-width: 768px) {
+      max-width: 50em;
+    }
+
+    // Tablets
+    @media (min-width: 769px) and (max-width: 1023px) {
+      margin-left: auto;
+      margin-right: auto;
+      min-width: 90%;
+    }
+
+    // Desktops
+    @media (min-width: 1024px) {
+      max-width: 50em;
+      margin-left: auto;
+      margin-right: auto;
+      min-width: 50%;
+    }
+  }
+</style>
---
+++ src/profiles/Planificateur.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  export let score = 0;
+</script>
+
+
+<h3>Le Planificateur [{score}]</h3>
+<h4>🎯 Points forts :</h4>
+<ul class="strengths">
+  <li># Sens de la réflexion, de l'analyse</li>
+  <li># Poursuite d'objectifs structurés</li>
+  <li># Scénarisation fine des actions</li>
+  <li># Goût pour l'anticipation et l'évaluation</li>
+</ul>
+<h4>📜 Description :</h4>
+<p>
+  S'il y a bien quelqu'un qui sait où il va, et comment il va y aller, c'est toi ! Et oui, avant d'agir, tu réfléchis, tu imagines, tu projettes les choses pour qu'elles se déroulent comme tu le souhaiterais. Que me faudra-t-il avoir ? Qui pourra m'aider ? Que dois-je savoir ou savoir faire ? Etc. Cela veut dire que tu as d'abord déterminé, de façon précise, ton ou tes objectifs. S'ensuit la mise au point d'un plan balisé en quelques grandes étapes. Bref, tu mets au point une véritable scénarisation de tes actions. Et tu définis souvent plusieurs scenarii (plan A, B, C …) pour t'assurer de tout anticiper, tout maîtriser. 
+</p>
+<p>
+  Une fois ton plan lancé, tu t'attaches à suivre, pas à pas, chacune des tâches prévues. Cela te permet de bien évaluer ton avancement et donc de vérifier si tu es dans les temps par rapport à tes prévisions de départ. Cette maîtrise de choses et de leur déroulement, mais aussi ton état permanent de vigilance sur ce qui se passe et pourrait mettre à mal tes plans, te permet d'absorber les obstacles et surprises rencontrés en effectuant rapidement les changements nécessaires. Cette flexibilité doit garantir la poursuite de ton objectif de départ.
+</p>
+<p>
+  Un conseil : ne reste pas trop absorbé par ton besoin de tout contrôler, lâche prise aussi, en accueillant les imprévus pour explorer autre chose, cela te permettra de combiner les talents du planificateur et du bricoleur, deux profils souvent opposés, alors qu'ils sont en réalité très complémentaires !
+</p>
+
+<style lang="scss">
+  h3 {
+    text-align: center;
+  }
+
+  ul.strengths {
+    padding-left: 0.25em;
+
+    li {
+      background-color: #800000;
+      padding: 0.25em 0.5em 0.25em 0.5em;
+      color: white;
+      border-radius: 1em;
+      width: -moz-fit-content;
+    }
+    li:not(:last-child) {
+      margin-bottom: 0.3em;
+    }
+  }
+</style>
