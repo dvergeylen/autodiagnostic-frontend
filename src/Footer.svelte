fc1c29f1519a51997cff2c74956593981c967696
Daniel Vergeylen <daniel.vergeylen@sigmadelta.io>
2021-11-08 11:49:25 +0100

    feat: added missing logo in footer

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,20 +1,35 @@
 <footer>
+  <div>
+    <img id="logo-fse" src="/assets/images/logo_FSE_Coq_Wallonie.svg" alt="Logo FSE et Wallonie"/>
+  </div>
   <div id="footer-container">
     <div>
-      <img id="sowalfin-creation" src="/assets/images/logo_sowalfin_creation.svg" alt="Sowalfin Création"/>
+      <p id="moto" class="is-uppercase">'Voyage au bout du monde' est un outil développé le groupe <a href="https://www.sowalfin.be">Sowalfin</a>.</p>
     </div>
     <div>
-      <p class="is-uppercase">'Voyage au bout du monde' est un outil développé le groupe <a href="https://www.sowalfin.be">Sowalfin</a>.</p>
+      <img id="sowalfin-creation" src="/assets/images/logo_sowalfin_creation.svg" alt="Sowalfin Création"/>
     </div>
   </div>
 </footer>
 
 <style lang="scss">
+  footer {
+    max-width: 50em;
+    min-width: 50%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 2em;
+  }
+  #logo-fse {
+    max-width: 12em;
+    margin: auto;
+    margin-left: 5%;
+  }
   #footer-container {
+    display:flex;
     justify-content: center;
     align-items: center;
-    margin-bottom: 3em;
-    margin-top: 2em;
+    margin: auto;
 
     div {
       text-align: center;
@@ -39,12 +54,13 @@
     width: 100%;
   }
 
-  #sowalfin-creation {
-    max-width: 10em;
-    margin:auto;
+  #moto {
+    font-size: 0.75em;
   }
-
-  .is-uppercase {
-    text-transform: uppercase;
+  #sowalfin-creation {
+    max-width: 5em;
+    margin:auto;
+    margin-left: 0.5em;
+    margin-right: 0.5em;
   }
 </style>
