18afce7a7b939b82b89237c49b4dc302bcb19e0f
Daniel Vergeylen <daniel.vergeylen@sigmadelta.io>
2021-11-16 12:55:44 +0100

    feat: update Footer to comply with Sowalfin policy

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,14 +1,17 @@
 <footer>
-  <div>
-    <img id="logo-fse" src="/assets/images/logo_FSE_Coq_Wallonie.svg" alt="Logo FSE et Wallonie"/>
-  </div>
-  <div id="footer-container">
-    <div>
-      <p id="moto" class="is-uppercase">'Voyage au bout du monde' est un outil développé le groupe <a href="https://www.sowalfin.be">Sowalfin</a>.</p>
-    </div>
-    <div>
+  <div class="is-flex has-align-items-center">
+    <div class="logo-container">
       <img id="sowalfin-creation" src="/assets/images/logo_sowalfin_creation.svg" alt="Sowalfin Création"/>
     </div>
+    <div class="logo-container">
+      <img src="/assets/images/logo_FSE_Coq_Wallonie.svg" alt="Logo FSE et Wallonie"/>
+    </div>
+    <div class="logo-container">
+      <img src="/assets/images/logo_EnMieux.svg" alt="Logo En Mieux"/>
+    </div>
+  </div>
+  <div id="footer-container">
+    <p id="moto" class="is-uppercase">'Voyage au bout du monde' est un outil développé le groupe <a href="https://www.sowalfin.be">Sowalfin</a>.</p>
   </div>
 </footer>
 
@@ -25,26 +28,6 @@
     margin: auto;
     margin-left: 5%;
   }
-  #footer-container {
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    margin: auto;
-
-    div {
-      text-align: center;
-    }
-
-    // Tablets, Desktops
-    @media (min-width: 768px) {
-      display: flex;
-
-      div {
-        margin-left: 1em;
-        margin-right: 1em;
-      }
-    }
-  }
   footer::after {
     content: "";
     height: 2em;
@@ -57,10 +40,24 @@
   #moto {
     font-size: 0.75em;
   }
+  .logo-container {
+    height: 3em;
+    width: 6em;
+    display: flex;
+  }
+  .logo-container:not(:last-child) {
+    padding-right: 0.5em;
+    border-right: 1px solid gray;
+    margin-right: 0.5em;
+  }
   #sowalfin-creation {
     max-width: 5em;
     margin:auto;
     margin-left: 0.5em;
     margin-right: 0.5em;
+  }
+  #footer-container {
+    max-width: 70%;
+    font-weight: 700;
   }
 </style>
